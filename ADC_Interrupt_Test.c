d7b63bec5ddd92f170d2750d2c00cc4c54f435d1
ryood <kawanabe@gmail.com>
2015-09-06 11:45:29 +0900

    ADC_Interrupt_Test.c: 表示するPOTを切り替えるSWを追加

--- ADC_Interrupt_Test.c
+++ ADC_Interrupt_Test.c
@@ -29,6 +29,60 @@
 volatile uint8_t pot_data[2];
 volatile uint8_t pot_n;
 
+volatile uint8_t user_sw;
+volatile uint8_t user_sw_rd;
+volatile uint8_t user_sw_data;
+
+//---------------------------------------------------------------------------------------------
+// Pin Change Interrupt & Debounce
+//
+void init_switches()
+{
+	// Pin Change Interruptの有効化
+	PCICR = (1 << PCIE0);
+	PCMSK0 = (1 << PB4);
+	
+	// TIMER0 オーバーフロー割り込みの有効化
+	TCCR0B = 0x00;	// Timer0停止
+	TIMSK0 = (1 << TOIE0);
+}
+
+ISR (TIMER0_OVF_vect)
+{
+	// 割り込みごとにLEDを点滅（デバッグ用）
+	PORTC ^= (1 << PC3);
+	
+	// Timer0を停止
+	TCCR0B = 0x00;
+
+	uint8_t tmp = (~PORTB & (1 << PB4));
+	if (tmp == user_sw_rd) {
+		user_sw = !user_sw;
+		if (user_sw != 0) {
+			// トグル動作
+			user_sw_data = user_sw_data ? 0 : 1;
+		}
+	}
+	
+	// Pin Change Interruptの有効化
+	PCICR = (1 << PCIE0);
+}
+
+ISR (PCINT0_vect)
+{
+	// Pin Change Interruptを無効化
+	PCICR = 0x00;
+	
+	user_sw_rd = (~PORTB & (1 << PB4));
+	
+	// Timer0を起動
+	TCCR0B = 0x05;	// プリスケーラ－:1024, 1/(8MHz/1024)=128us
+	TCNT0 = 176;	// 128us*(256-176)=10.24ms
+}
+
+//---------------------------------------------------------------------------------------------
+// ADC
+//
 ISR(ADC_vect)
 {
 	switch (pot_n) {
@@ -51,6 +105,9 @@ ISR(ADC_vect)
 	}
 }
 
+//---------------------------------------------------------------------------------------------
+// Main routine
+//
 int main(void)
 {
 	DDRB = 0x00;
@@ -59,12 +116,24 @@ int main(void)
 
 	// LED
 	DDRD |= 0xFF;
+	DDRB |= (1 << PB5);
+	DDRC |= (1 << PC3);
 	
 	// LED Check
+	PORTB |= (1 << PB5);
+	PORTC |= (1 << PC3);
 	for (int i = 0; i <= 8; i++) {
 		PORTD = (0xFF >> i);
 		_delay_ms(100);
 	}
+	PORTB &= ~(1 << PB5);
+	_delay_ms(100);
+	PORTC &= ~(1 << PC3);
+	
+	// Switch input/pullup
+	PORTB |= (1 << PB4);
+	
+	init_switches();
 	
 	// Potentiometer
 	// Enable the ADC and its interrupt feature
@@ -81,9 +150,20 @@ int main(void)
 	ADCSRA |= (1 << ADSC);		// Start Conversion
 	
 	while(1) {
+		/*
 		PORTD = pot_data[0];
 		_delay_ms(1000);
 		PORTD = pot_data[1];
 		_delay_ms(1000);
+		*/
+		
+		switch (user_sw_data) {
+		case 0:
+			PORTB &= ~(1 << PB5);
+			break;
+		case 1:
+			PORTB |= (1 << PB5);
+			break;
+		}
 	}
 }
